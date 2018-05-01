472e2a4556b2fad689818e4c55c582776870e702
mwoolweaver <m.woolweaver@icloud.com>
2018-04-30 22:10:40 -0500

    maybe now automation?

---
+++ scripts/update.sh
@@ -0,0 +1,6 @@
+function update(){
+  wget https://raw.githubusercontent.com/uikit/uikit/develop/dist/css/uikit.min.css -O ./css/uikit.min.css
+  wget https://raw.githubusercontent.com/uikit/uikit/develop/dist/js/uikit-icons.min.js -O ./js/uikit-icons.min.js
+  wget https://raw.githubusercontent.com/uikit/uikit/develop/dist/js/uikit.min.js -O ./js/uikit.min.js
+  return 0;
+}
